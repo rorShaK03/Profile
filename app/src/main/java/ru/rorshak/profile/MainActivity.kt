b96de53d17362bd0ff7cef39f69af6eb00a801cc
Kirill <shabad.k@yandex.ru>
2021-07-23 17:18:13 +0300

    1 step

--- app/src/main/java/ru/rorshak/profile/MainActivity.kt
+++ app/src/main/java/ru/rorshak/profile/MainActivity.kt
@@ -1,11 +1,29 @@
 package ru.rorshak.profile
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import ru.rorshak.profile.adapter.ItemsAdapter
+import ru.rorshak.profile.databinding.ActivityMainBinding
+import ru.rorshak.profile.model.Item
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var itemsAdapter : ItemsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        itemsAdapter = ItemsAdapter()
+        with(viewBinding.recycler)
+        {
+            layoutManager = LinearLayoutManager(context)
+            adapter = itemsAdapter
+        }
+        itemsAdapter.items = listOf(Item.Photo(""), Item.Desc(""), Item.Header("Навыки"), Item.Skill("Kotlin", "<1 года"),
+        Item.Skill("C++", "2 года"), Item.Skill("Python", "3 года"), Item.Skill("Assembler", "менее  1 года"))
     }
 }
---
+++ app/src/main/java/ru/rorshak/profile/adapter/ItemsAdapter.kt
@@ -0,0 +1,132 @@
+package ru.rorshak.profile.adapter
+
+import android.content.ClipData
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import ru.rorshak.profile.databinding.*
+import ru.rorshak.profile.model.Item
+import java.lang.IllegalArgumentException
+
+class ItemsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var items: List<Item> = emptyList()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            VIEW_TYPE_HEADER ->
+                ViewHolderHeader(
+                    ItemHeaderBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            VIEW_TYPE_TEXT ->
+                ViewHolderText(
+                    ItemTextBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            VIEW_TYPE_PHOTO ->
+                ViewHolderPhoto(
+                    ItemPhotoBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            VIEW_TYPE_DESC ->
+                ViewHolderDesc(
+                    ItemDescBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                    parent,
+                    false)
+                )
+            VIEW_TYPE_SKILL ->
+                ViewHolderSkill(
+                    ItemSkillBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false)
+                )
+            else -> throw IllegalArgumentException("")
+        }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (val item = items[position]) {
+            is Item.Header -> (holder as? ViewHolderHeader)?.onBind(item)
+            is Item.withText -> (holder as? ViewHolderText)?.onBind(item)
+            is Item.Skill -> (holder as? ViewHolderSkill)?.onBind(item)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int =
+        when (items[position]) {
+            is Item.Header -> VIEW_TYPE_HEADER
+            is Item.withText -> VIEW_TYPE_TEXT
+            is Item.Photo -> VIEW_TYPE_PHOTO
+            is Item.Desc -> VIEW_TYPE_DESC
+            is Item.Skill -> VIEW_TYPE_SKILL
+        }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolderSkill(
+        private val viewBinding: ItemSkillBinding
+    ) : RecyclerView.ViewHolder(viewBinding.root)
+    {
+        fun onBind(item : Item.Skill)
+        {
+            viewBinding.skill.text = item.name
+            viewBinding.time.text = item.time
+        }
+    }
+
+    class ViewHolderDesc(
+        private val viewBinding: ItemDescBinding
+    ) : RecyclerView.ViewHolder(viewBinding.root)
+    {
+
+    }
+
+    class ViewHolderPhoto(
+        private val viewBinding: ItemPhotoBinding
+    ) : RecyclerView.ViewHolder(viewBinding.root)
+    {
+        
+    }
+
+    class ViewHolderHeader(
+        private val viewBinding: ItemHeaderBinding
+    ) : RecyclerView.ViewHolder(viewBinding.root) {
+        fun onBind(item: Item.Header) {
+            viewBinding.header.text = item.title
+        }
+    }
+
+    class ViewHolderText(
+        private val viewBinding: ItemTextBinding
+    ) : RecyclerView.ViewHolder(viewBinding.root) {
+        fun onBind(item: Item.withText) {
+            viewBinding.text.text = item.text
+        }
+    }
+
+    companion object {
+        private const val VIEW_TYPE_HEADER = 1
+        private const val VIEW_TYPE_TEXT = 2
+        private const val VIEW_TYPE_PHOTO = 3
+        private const val VIEW_TYPE_DESC = 4
+        private const val VIEW_TYPE_SKILL = 5
+    }
+}
